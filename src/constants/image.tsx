44e0c46c42d7e2c46bd92262061dc8cbe8a2f187
Aqib-Dev-webrange <aqib.ahmed@webrangesolutions.com>
2025-06-05 15:26:16 +0500

    add the images

---
+++ src/constants/image.tsx
@@ -0,0 +1,16 @@
+
+
+export const IMAGES = {
+  react: "/images/logo_light.svg",
+  node: "/images/node.svg",
+  next: "/next.svg",
+  express: "/images/express.svg",
+  typescript: "/images/ts.png",
+  javascript: "/images/js.png",
+  tailwind: "/images/tailwind.svg",
+  mongodb: "/images/mongodb-logo.png",
+  mysql: "/images/mysql.svg",
+  docker: "/images/docker.png",
+  sequelize: "/images/sequalize.png",
+  s3: "/images/amazon-s3.svg",
+}
